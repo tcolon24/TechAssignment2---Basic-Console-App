f03ca256d0c843d0036b2c53246918556403cfe4
Tyler <Tyler@DESKTOP-OH34BO4>
2022-01-25 11:52:07 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TechAssignment2___Basic_Console_App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello My name is Tyler Colon and going off of one technology quote you like. One I found interesting is: Our future success is directly porportional to our ability to understand, adopt and integrate new technology into our work. -Sukant Ratnakar");
+        }
+    }
+}
